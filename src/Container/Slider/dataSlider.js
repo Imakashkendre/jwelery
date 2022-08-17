3ca2e16f5c2bd55b34f7351bdd975d87f4061c0c
Imakashkendre <akash.shopisthan@gmail.com>
2022-08-17 15:11:00 +0530

    shop container

---
+++ src/Container/Slider/dataSlider.js
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    url: "https://images.unsplash.com/photo-1599907888904-14d5d22bfe19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    url: "../../Assets/Images/cornelia-ng-2zHQhfEpisc-unsplash.jpg",
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    url: "../../Assets/Images/tasaveeer-studio-ffPis2pqbUg-unsplash.jpg",
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    url: "../../Assets/Images/edgar-soto-gb0BZGae1Nk-unsplash.jpg",
+  },
+
+];
+
+export default dataSlider;
