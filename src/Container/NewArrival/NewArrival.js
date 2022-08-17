75ffef483144e7742a62fc76581eb74509b9e1d3
Imakashkendre <akash.shopisthan@gmail.com>
2022-08-17 16:59:12 +0530

    new arrival update

--- src/Container/NewArrival/NewArrival.js
+++ src/Container/NewArrival/NewArrival.js
@@ -9,22 +9,36 @@ const NewArrival = () => {
     return (
         <>
             {/* heading div for new arrival  */}
-            <div className='heading-div-for-arrivals'><h2>Timeless design that told you unique story</h2></div>
+            <div className='heading-div-for-arrivals'><h2>Timeless design that told you unique story</h2>
+                <div>
+                    <p className='para-after-heading'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam adipisicing elit. Dolor totamadipis </p>
+                </div>
+            </div>
             <div>
+
+                <div className='newarrivval-heading'><h2>New Arrival</h2></div>
                 <div className='wid-div'>
                     <div>
                         <img src={img1} alt="" className='imgfirst-style' />
+                        <p>Gold Earings</p>
+                        <p>Rs.600</p>
                     </div>
                     <div>
                         <div className='div-for-sub-div'>
                             <div>
                                 <img src={img4} alt="" className="subdiv-img" />
+                                <p>Gold Earings</p>
+                                <p>Rs.600</p>
                             </div>
                             <div>
                                 <img src={img3} alt="" className="subdiv-img" />
+                                <p>Gold Earings</p>
+                                <p>Rs.600</p>
                             </div>
                         </div>
-                        <div><img src={img2} alt="" className="second-div-of-sub-div-for-img" /></div>
+                        <div><img src={img2} alt="" className="second-div-of-sub-div-for-img" />
+                            <p>Gold Earings</p>
+                            <p>Rs.600</p></div>
                     </div>
 
                 </div>
